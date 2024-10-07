ee13db0b1078a593a722eb6a93002f62404c59a6
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-06 23:42:02 -0500

    uso del hookk - useState

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,22 +1,30 @@
 import { useState } from "react"
 
-const CounterApp = () => {
+export const CounterApp = () => {
 
-    const [counter, setCounter] = useState(10)
+    const [state, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    })
+
+    const { counter1, counter2, counter3 } = state;
 
     return (
         <>
-            <h1>Counter: {counter} </h1>
+            <h1>Counter: {counter1} </h1>
+            <h1>Counter: {counter2} </h1>
+            <h1>Counter: {counter3} </h1>
 
             <hr />
 
             <button
-                onClick={() => setCounter(counter + 1)}
+                onClick={() => setCounter({
+                    ...state, counter1: counter1 + 1
+                })}
                 className="btn btn-primary">
                 +1
             </button>
         </>
     )
 }
-
-export default CounterApp
