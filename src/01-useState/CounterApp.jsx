d24b3a88475b7925e05c69a359d82f67db08130c
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-06 23:26:16 -0500

    Inicio de proyecto - HooksApp

---
+++ src/01-useState/CounterApp.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+const CounterApp = () => {
+
+    const [counter, setCounter] = useState(10)
+
+    return (
+        <>
+            <h1>Counter: {counter} </h1>
+
+            <hr />
+
+            <button
+                onClick={() => setCounter(counter + 1)}
+                className="btn btn-primary">
+                +1
+            </button>
+        </>
+    )
+}
+
+export default CounterApp
