62145b0a5a17a95cdd0ddf8172b5aecf84944258
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-07 22:01:57 -0500

    useEffect - SimpleForm

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'andres',
+        email: 'arevelo.edu@dev.com'
+    });
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+
+    useEffect(() => {
+        console.log('useEffect called');
+    }, [])
+
+    return (
+        <>
+            <h1>Formulario Simple</h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="arevelo.edu@gmail.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import HooksApp from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 import './index.css'
 
 
@@ -10,7 +11,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
-    <CounterWithCustomHook />
+    {/* <CounterWithCustomHook /> */}
+    <SimpleForm />
 
   </StrictMode>,
 )
