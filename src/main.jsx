c5e0101961f4af895c9f0c203b47875486a34a6e
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-09 22:14:12 -0500

    Memo - Método de React

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,9 @@ import { createRoot } from 'react-dom/client'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
+// import { Layout } from './05-useLayoutEffect/Layout'
 import './index.css'
+import { Memorize } from './06-memos/Memorize'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,7 +20,8 @@ createRoot(document.getElementById('root')).render(
     {/* <FormWithCustomHook /> */}
     {/* <MultipleCustomHooks /> */}
     {/* <FocusScreen/> */}
-    <Layout/>
+    {/* <Layout/> */}
+    <Memorize />
 
   </StrictMode>,
 )
