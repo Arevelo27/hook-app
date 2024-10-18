02e214ed1d82c3bc3a77f9a9aad73dc4d5dd6004
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-17 22:13:01 -0500

    Ejecutar funciones del customHook dentro de las pruebas

--- test/hooks/useCounter.test.js
+++ test/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useCounter } from "../../src/hooks";
 
 describe('Pruebas en el useCounter', () => {
@@ -18,4 +18,39 @@ describe('Pruebas en el useCounter', () => {
         const { counter } = result.current;
         expect(counter).toBe(100);
     });
+
+    test('debe de incrementar el counter', () => {
+        const { result } = renderHook(() => useCounter(100));
+        const { increment } = result.current;
+        act(() => {
+            increment();
+            increment(2);
+        });
+        const { counter } = result.current;
+        expect(counter).toBe(103);
+    });
+
+    test('debe de decrementar el counter', () => {
+        const { result } = renderHook(() => useCounter(100));
+        const { decrement } = result.current;
+        act(() => {
+            decrement();
+            decrement(2);
+        });
+        const { counter } = result.current;
+        expect(counter).toBe(97);
+    });
+
+    test('debe de resetear el counter', () => { 
+        const { result } = renderHook(() => useCounter(100));
+        const { decrement, reset } = result.current;
+        act(() => {
+            decrement();
+            decrement();
+            reset();
+        });
+        const { counter } = result.current;
+        expect(counter).toBe(100);
+    });
+
 })
